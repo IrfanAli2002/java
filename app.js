7a28d0bc2ca6747f5a4dba6c0921ce56378088ad
IrfanAli2002 <94832900+IrfanAli2002@users.noreply.github.com>
2021-12-09 16:23:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,193 @@
+// CH # 1
+// alert("Hello")
+// alert("welcome to JS land")
+// console.log("How are you?")
+// CH # 1 end
+
+
+// CH # 2
+
+
+// var username = ("irfan ali baig")
+// var mybio = ("19 years old")
+// var designation = ("wed developer")
+// alert(( username)
+// alert(mybio)
+// alert(designation)
+// var a = ["pizza","pizz","piz","pi","p"]
+// alert(a)
+// var email = ("email123@gmail.com")
+// alert(email)
+// var b = ("a smarter way to learn java script")
+// alert(b)
+// var docu = ("Yah! i can write HTML content through  java script")
+// document.write(docu)
+// var a = ("_______________________%%$$%%____________________")
+// alert(a)
+
+
+// CH # 2 end
+
+// CH # 3 start
+
+// var age = ("I am 19 years old")
+// alert(age)
+// var a = ("you have visited 2 times this times")
+// alert(a)
+// var b = ("My birth year is 2000")
+// var a = ("data type of this varible is number")
+// document.write(b)
+// document.write(a)
+// var c = ("John Doe ordered-5 T-shirt(s) on XYZ Clothing store")
+// document.write(c)
+
+
+// CH # 3 end
+
+// CH # 4 start
+
+// var firsT = ("Rules for naming JS variables”")
+// var firsTvaR = ("Variable names can only contain")
+// var FirsTvariBle = (" Variables must begin with a")
+// var se_condvar = ("Variable names are case")
+// var $secondvariBle = ("Variable names should not be JS")
+
+// document.write(firsT)
+
+// document.write(firsTvaR)
+// document.write(FirsTvariBle)
+// document.write(se_condvar)
+// document.write($secondvariBle)
+
+// CH # 4 end
+
+
+// CH # 5 start
+
+// var a = 2;
+// var b = 3;
+
+// alert(a+b)
+
+// var a = 2;
+// var b = 3;
+
+// alert(a*b)
+// var a = 3;
+// var b = 3;
+
+// alert(a/b)
+
+// var a = 10;
+// var b = 4;
+
+// alert(a-b)
+
+
+// var a = 4;
+// for(var i =1; i <=10; i++){
+    //     document.write(a *i)
+    // }
+    
+    
+    // var a = 650;
+    // var b = +prompt("how many items you want?")
+    // var c = 100;
+    // var d = +prompt("how many items you want?")
+    // var total = (a*b) + (c*d)
+    // document.write(total)
+    
+    
+    // var a = 1000;
+    // var b = +prompt("how many numbers you get?")
+    // var total = (b/1000*100)
+    // alert(total)
+    
+    // var usdollar = 104;
+    // var pakistanirupee = +prompt("how many dollar u have?")
+    // var PKR = (usdollar*pakistanirupee)
+    // alert(PKR)
+    
+    // CH # 5 end
+
+
+    // CH 6 to 9 start
+    
+    // var a = 2;
+    // var b = a++;
+    // alert(a)
+    
+    // var a = 2;
+    // var b = a--;
+    // alert(a)
+    
+    
+    
+    // var a = 2;
+    // var b = ++a;
+    // alert(a)
+    
+    // var a = 2;
+    // var b = 1;
+    
+    // var c   = --a - --b + ++b + b--;
+    
+    // document.write(c)
+    // console.log("value of a=1 (value of b=10)")
+
+
+    // var eng = +prompt("english marks 100 (enter your marks)")
+    // var urd = +prompt("urdu marks 100(enter your marks)")
+    // var sci = +prompt("science marks 100(enter your marks)")
+    // var total = 300;
+    // var uSermarks = (eng+urd+sci)
+    // alert(uSermarks/total*100)
+    
+    // CH 6 to 9 end
+    
+    
+    // var a = ("karachi")
+    // var b = ("isl")
+    // var c = ("lahore")
+    // var user = prompt("enter your city")
+    
+    
+    // if(user == a){
+    //     alert("welcome the city of light")
+    // }
+    // else alert ("sorry you are not allowed")
+    
+//     var a = "red"
+//     var b = "yellow"
+//     var c = "green"
+    
+//     var user = prompt("enter color")
+
+//     if(user == a){
+//         alert("Must stop")
+//     }
+//     if(user == b){
+//         alert("ready to move")
+//     }
+//     if(user == c){
+//         alert("move now")
+//     }
+//     else {
+//         alert("sorry this is not valid")
+// }
+ 
+
+
+    
+    
+
+
+
+
+    
+
+    
+    
+    
+
+
